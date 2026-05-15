2bed44eb2afbf02b1da3fbe3f519a915d3c5b438
Anton Kulikov <kulikov.anton.dev@proton.me>
2026-05-15 09:00:47 +0700

    R Occurrence counter

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,63 +2,43 @@ import java.util.*;
 import java.util.concurrent.*;
 
 public class Main {
+    public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
 
-    public static void main(String[] args) throws InterruptedException, ExecutionException {
-        String[] texts = new String[25];
-        for (int i = 0; i < texts.length; i++) {
-            texts[i] = generateText("aab", 30_000);
-        }
-
-        List<Future<Integer>> futureList = new ArrayList<>();
+    public static void main(String[] args) throws InterruptedException {
         ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-        for (String text : texts) {
-            Callable<Integer> tester = () -> {
-                return checkOneText(text);
-            };
-            futureList.add(executor.submit(tester));
+        for (int i = 0; i < 1000; i++) {
+            executor.execute(Main::computeROccurrence);
         }
         executor.shutdown();
-
-        int maxInterval = 0;
-        long startTs = System.currentTimeMillis(); // start time
-        for (Future<Integer> future : futureList) {
-            int tmp = future.get();
-            maxInterval = Math.max(tmp, maxInterval);
+        executor.awaitTermination(Long.MAX_VALUE, TimeUnit.MILLISECONDS);
+        Map.Entry<Integer, Integer> maxSizeToFreq = Collections.max(sizeToFreq.entrySet(), Map.Entry.comparingByValue());
+        System.out.println("Max sequence of " + maxSizeToFreq.getKey() + " Rs was found " + maxSizeToFreq.getValue() + " times.\n Other occurrences: ");
+        for (var entry : sizeToFreq.entrySet()) {
+            System.out.println(" - " + entry.getKey() + " (" + entry.getValue() + " times)");
         }
-        long endTs = System.currentTimeMillis(); // end time
 
-        System.out.println("Max interval: " + maxInterval);
-        System.out.println("Time: " + (endTs - startTs) + "ms");
     }
 
-    public static int checkOneText(String text) {
-            int maxSize = 0;
-            for (int i = 0; i < text.length(); i++) {
-                for (int j = 0; j < text.length(); j++) {
-                    if (i >= j) {
-                        continue;
-                    }
-                    boolean bFound = false;
-                    for (int k = i; k < j; k++) {
-                        if (text.charAt(k) == 'b') {
-                            bFound = true;
-                            break;
-                        }
-                    }
-                    if (!bFound && maxSize < j - i) {
-                        maxSize = j - i;
-                    }
-                }
-            }
-            return maxSize;
-    }
-
-    public static String generateText(String letters, int length) {
+    public static String generateRoute(String letters, int length) {
         Random random = new Random();
-        StringBuilder text = new StringBuilder();
+        StringBuilder route = new StringBuilder();
         for (int i = 0; i < length; i++) {
-            text.append(letters.charAt(random.nextInt(letters.length())));
+            route.append(letters.charAt(random.nextInt(letters.length())));
         }
-        return text.toString();
+        return route.toString();
+    }
+
+    static void computeROccurrence() {
+        final String seq = generateRoute("RLRFR", 100);
+        int counter = 0;
+        for (int i = 0; i < seq.length(); i++) {
+            if (seq.charAt(i) == 'R') {
+                counter++;
+            }
+        }
+        synchronized (sizeToFreq) {
+            int currentFreq = sizeToFreq.getOrDefault(counter, 0);
+            sizeToFreq.put(counter, ++currentFreq);
+        }
     }
 }
