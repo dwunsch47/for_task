88a5cf0707663d146c2f437799498f33be770190
Anton Kulikov <kulikov.anton.dev@proton.me>
2026-05-22 00:33:23 +0700

    Add stub and tests for add()

--- src/main/java/Phonebook.java
+++ src/main/java/Phonebook.java
@@ -1,2 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 public class Phonebook {
+
+    SortedMap<String, String> nameToPhone = new TreeMap<>();
+    Map<String, String> phoneToName = new HashMap<>();
+
+    public int add(String name, String number) {
+        return 0;
+    }
 }
--- src/test/java/PhonebookTest.java
+++ src/test/java/PhonebookTest.java
@@ -1,2 +1,20 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class PhonebookTest {
+
+    static Phonebook pb = null;
+
+    @BeforeEach
+    void startup() {
+        pb = new Phonebook();
+    }
+
+    @Test
+    void givenNameAndNumber_whenAdd_thenReturnNumberOfContacts() {
+        int result = pb.add("Name", "88005553535");
+        Assertions.assertEquals(1, result);
+    }
 }
